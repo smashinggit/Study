fb58cfe1195312831ef1464edd356c68962d1676
chensen <chensen@cmos.chinamobile.com>
2019-05-22 17:44:17 +0800

    Agera 2

--- AgeraDemo/src/main/java/com/cmos/agerademo/Test.java
+++ AgeraDemo/src/main/java/com/cmos/agerademo/Test.java
@@ -4,8 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
+import com.google.android.agera.Function;
+import com.google.android.agera.Merger;
 import com.google.android.agera.Observable;
+import com.google.android.agera.Receiver;
+import com.google.android.agera.Repositories;
+import com.google.android.agera.Repository;
+import com.google.android.agera.RepositoryConfig;
+import com.google.android.agera.Supplier;
 import com.google.android.agera.Updatable;
 
 /**
@@ -15,6 +23,53 @@ import com.google.android.agera.Updatable;
  */
 public class Test extends Activity {
 
+    Repository<String> repository =
+            Repositories.repositoryWithInitialValue("init")
+                    .observe()
+                    .onUpdatesPerLoop()
+                    .getFrom(new Supplier<Float>() {
+                        @NonNull
+                        @Override
+                        public Float get() {
+                            return 50000000.00f;
+                        }
+                    })
+                    .mergeIn(new Supplier<String>() {
+                        @NonNull
+                        @Override
+                        public String get() {
+                            return "祝大家的银行卡里余额为：";
+                        }
+                    }, new Merger<Float, String, String>() {
+                        @NonNull
+                        @Override
+                        public String merge(@NonNull Float integer, @NonNull String tAdd) {
+                            return tAdd + integer;
+                        }
+                    })
+                    .sendTo(new Receiver<String>() {
+                        @Override
+                        public void accept(@NonNull String value) {
+                            Log.d("tag", value);
+                        }
+                    })
+                    .thenTransform(new Function<String, String>() {
+                        @NonNull
+                        @Override
+                        public String apply(@NonNull String input) {
+                            return input + " 吼吼吼~";
+                        }
+                    })
+                    .onDeactivation(RepositoryConfig.CANCEL_FLOW)
+                    .onConcurrentUpdate(RepositoryConfig.CANCEL_FLOW)
+                    .compile();
+
+    Updatable updatable = new Updatable() {
+        @Override
+        public void update() {
+            Log.d("tag", repository.get());
+        }
+    };
 
 
     @Override
@@ -25,10 +80,12 @@ public class Test extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        repository.addUpdatable(updatable);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        repository.removeUpdatable(updatable);
     }
 }
